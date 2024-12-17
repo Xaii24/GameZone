8c92199999c15f683082bc47909891b1a4caca07
Xaii24 <110148784+Xaii24@users.noreply.github.com>
2024-12-16 22:48:35 -0700

    Added DebugKit route

--- config/routes.php
+++ config/routes.php
@@ -18,6 +18,14 @@ $routes->scope('/', function (RouteBuilder $builder) {
 
     $builder->fallbacks();
 });
+
+// DebugKit route
+$routes->plugin('DebugKit', ['path' => '/debug-kit'], function (
+    RouteBuilder $routes
+) {
+    $routes->fallbacks();
+});
+
 $routes->connect('/articles/delete/:slug', [
     'controller' => 'Articles',
     'action' => 'delete',
