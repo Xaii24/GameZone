3c2df8727b15952748a02630312027e69e5f98d5
Xaii24 <110148784+Xaii24@users.noreply.github.com>
2025-02-02 02:40:43 -0700

    Add up() and down() methods to migration

--- config/Migrations/20240731192654_AddLikesCountToArticles.php
+++ config/Migrations/20240731192654_AddLikesCountToArticles.php
@@ -5,11 +5,20 @@ use Migrations\AbstractMigration;
 
 class AddLikesCountToArticles extends AbstractMigration
 {
-    /**
- * Change Method.
- *
- * More information on this method is available here:
- * https://book.cakephp.org/phinx/0/en/migrations.html#the-change-method
- * @return void
- */
+    public function up()
+    {
+        $table = $this->table('articles');
+        $table
+            ->addColumn('likes_count', 'integer', [
+                'default' => 0,
+                'null' => false,
+            ])
+            ->update();
+    }
+
+    public function down()
+    {
+        $table = $this->table('articles');
+        $table->removeColumn('likes_count')->update();
+    }
 }
