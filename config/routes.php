01b9feaf6adbf1f255b40543ca4d30d6e1230b11
Xaii24 <110148784+Xaii24@users.noreply.github.com>
2025-02-04 00:57:20 -0700

    Updated homepage route to ArticlesController

--- config/routes.php
+++ config/routes.php
@@ -5,26 +5,28 @@ use Cake\Routing\RouteBuilder;
 $routes->setRouteClass(DashedRoute::class);
 
 $routes->scope('/', function (RouteBuilder $builder) {
+    // Set the homepage to be handled by ArticlesController -> index
     $builder->connect('/', [
-        'controller' => 'Pages',
-        'action' => 'display',
-        'login',
+        'controller' => 'Articles',
+        'action' => 'index',
     ]);
 
+    // Add other routes here
+    $builder->connect('/articles/delete/:slug', [
+        'controller' => 'Articles',
+        'action' => 'delete',
+    ]);
+
+    $builder->connect('/articles/view/:slug', [
+        'controller' => 'Articles',
+        'action' => 'view',
+    ]);
+
+    $builder->connect('/comments/view/:id', [
+        'controller' => 'Comments',
+        'action' => 'view',
+    ]);
+
+    // Fallback route (default route for other controllers)
     $builder->fallbacks();
 });
-
-$routes->connect('/articles/delete/:slug', [
-    'controller' => 'Articles',
-    'action' => 'delete',
-]);
-
-$routes->connect('/articles/view/:slug', [
-    'controller' => 'Articles',
-    'action' => 'view',
-]);
-
-$routes->connect('/comments/view/:id', [
-    'controller' => 'Comments',
-    'action' => 'view',
-]);
