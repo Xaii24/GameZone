f16080aee54a81f1b346224f9a50f7e11d9f7994
Xaii24 <110148784+Xaii24@users.noreply.github.com>
2025-02-04 13:27:32 -0700

    Fix missing routes

--- config/routes.php
+++ config/routes.php
@@ -5,21 +5,49 @@ use Cake\Routing\RouteBuilder;
 $routes->setRouteClass(DashedRoute::class);
 
 $routes->scope('/', function (RouteBuilder $builder) {
+    // Set homepage (Change if needed)
     $builder->connect('/', [
-        'controller' => 'Pages',
-        'action' => 'display',
-        'login',
+        'controller' => 'Articles',
+        'action' => 'index',
     ]);
 
+    // Pages routes
     $builder->connect('/pages/*', [
         'controller' => 'Pages',
         'action' => 'display',
     ]);
 
+    // Articles routes
+    $builder
+        ->connect('/articles/view/:slug', [
+            'controller' => 'Articles',
+            'action' => 'view',
+        ])
+        ->setPass(['slug']);
+
+    $builder
+        ->connect('/articles/delete/:slug', [
+            'controller' => 'Articles',
+            'action' => 'delete',
+        ])
+        ->setPass(['slug']);
+
+    // Comments routes
+    $builder
+        ->connect('/comments/view/:id', [
+            'controller' => 'Comments',
+            'action' => 'view',
+        ])
+        ->setPass(['id']);
+
+    // Comment Likes Route
+    $builder
+        ->connect('/comments/like/:id', [
+            'controller' => 'CommentLikes',
+            'action' => 'add',
+        ])
+        ->setPass(['id']);
+
+    // Fallback routes
     $builder->fallbacks();
 });
-
-$routes->connect('/articles/delete/:slug', [
-    'controller' => 'Articles',
-    'action' => 'delete',
-]);
