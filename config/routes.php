152c50b6a45fa9a4fb3202ea701ebd563fd1eda6
Xaii24 <110148784+Xaii24@users.noreply.github.com>
2025-02-04 03:03:38 -0700

    Redirect back to article view after liking a comment

--- config/routes.php
+++ config/routes.php
@@ -5,29 +5,31 @@ use Cake\Routing\RouteBuilder;
 $routes->setRouteClass(DashedRoute::class);
 
 $routes->scope('/', function (RouteBuilder $builder) {
-    // Set the homepage to be handled by ArticlesController -> index
     $builder->connect('/', [
-        'controller' => 'Pages', // Use Pages controller (or your choice) for dynamic rendering
+        'controller' => 'Pages',
         'action' => 'display',
-        'home', // This will match the 'home' view file inside Pages
+        'login',
     ]);
 
-    // Add other routes here
-    $builder->connect('/articles/delete/:slug', [
-        'controller' => 'Articles',
-        'action' => 'delete',
+    $builder->connect('/pages/*', [
+        'controller' => 'Pages',
+        'action' => 'display',
     ]);
 
-    $builder->connect('/articles/view/:slug', [
-        'controller' => 'Articles',
-        'action' => 'view',
-    ]);
-
-    $builder->connect('/comments/view/:id', [
-        'controller' => 'Comments',
-        'action' => 'view',
-    ]);
-
-    // Fallback route (default route for other controllers)
     $builder->fallbacks();
 });
+
+$routes->connect('/articles/delete/:slug', [
+    'controller' => 'Articles',
+    'action' => 'delete',
+]);
+
+$routes->connect('/articles/view/:slug', [
+    'controller' => 'Articles',
+    'action' => 'view',
+]);
+
+$routes->connect('/comments/view/:id', [
+    'controller' => 'Comments',
+    'action' => 'view',
+]);
