319b7b3d0df21027fa5abca522d72798ba2d5fc2
Xaii24 <110148784+Xaii24@users.noreply.github.com>
2025-02-04 14:58:21 -0700

    Set login as default route

--- config/routes.php
+++ config/routes.php
@@ -5,21 +5,34 @@ use Cake\Routing\RouteBuilder;
 $routes->setRouteClass(DashedRoute::class);
 
 $routes->scope('/', function (RouteBuilder $builder) {
+    // Force the homepage to go to UsersController->login
     $builder->connect('/', [
-        'controller' => 'Pages',
-        'action' => 'display',
-        'login',
+        'controller' => 'Users',
+        'action' => 'login',
     ]);
 
-    $builder->connect('/pages/*', [
-        'controller' => 'Pages',
-        'action' => 'display',
+    // Define other specific routes
+    $builder
+        ->connect('/articles/view/:slug', [
+            'controller' => 'Articles',
+            'action' => 'view',
+        ])
+        ->setPass(['slug']);
+
+    $builder
+        ->connect('/articles/delete/:slug', [
+            'controller' => 'Articles',
+            'action' => 'delete',
+        ])
+        ->setPass(['slug']);
+
+    // If you want a logout route
+    $builder->connect('/logout', [
+        'controller' => 'Users',
+        'action' => 'logout',
     ]);
 
-    $builder->fallbacks();
+    // Disable automatic fallback routing (so CakePHP doesn’t interfere)
+    // Comment out this line if you want full manual control:
+    // $builder->fallbacks();
 });
-
-$routes->connect('/articles/delete/:slug', [
-    'controller' => 'Articles',
-    'action' => 'delete',
-]);
