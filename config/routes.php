5c5fbb25445bdd614dc2931e14cbb81989f0c2d1
Xaii24 <110148784+Xaii24@users.noreply.github.com>
2025-02-04 15:10:40 -0700

    Fixed routing issue and added comment like redirect

--- config/routes.php
+++ config/routes.php
@@ -5,34 +5,19 @@ use Cake\Routing\RouteBuilder;
 $routes->setRouteClass(DashedRoute::class);
 
 $routes->scope('/', function (RouteBuilder $builder) {
-    // Force the homepage to go to UsersController->login
     $builder->connect('/', [
-        'controller' => 'Users',
-        'action' => 'login',
+        'controller' => 'Pages',
+        'action' => 'display',
+        'login',
     ]);
 
-    // Define other specific routes
-    $builder
-        ->connect('/articles/view/:slug', [
-            'controller' => 'Articles',
-            'action' => 'view',
-        ])
-        ->setPass(['slug']);
-
-    $builder
-        ->connect('/articles/delete/:slug', [
-            'controller' => 'Articles',
-            'action' => 'delete',
-        ])
-        ->setPass(['slug']);
-
-    // If you want a logout route
-    $builder->connect('/logout', [
-        'controller' => 'Users',
-        'action' => 'logout',
+    $builder->connect('/pages/*', [
+        'controller' => 'Pages',
+        'action' => 'display',
     ]);
-
-    // Disable automatic fallback routing (so CakePHP doesn’t interfere)
-    // Comment out this line if you want full manual control:
-    // $builder->fallbacks();
 });
+
+$routes->connect('/articles/delete/:slug', [
+    'controller' => 'Articles',
+    'action' => 'delete',
+]);
