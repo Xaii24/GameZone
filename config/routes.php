d19f433ee1065ccb31fdfa52f23c5795da9e68f5
Xaii24 <110148784+Xaii24@users.noreply.github.com>
2025-02-04 03:14:49 -0700

    Fix routing for home page

--- config/routes.php
+++ config/routes.php
@@ -5,31 +5,42 @@ use Cake\Routing\RouteBuilder;
 $routes->setRouteClass(DashedRoute::class);
 
 $routes->scope('/', function (RouteBuilder $builder) {
+    // Fallback route for the homepage, allowing dynamic control
     $builder->connect('/', [
-        'controller' => 'Pages',
+        'controller' => 'Pages', // Use Pages controller (or your choice) for dynamic rendering
         'action' => 'display',
-        'login',
+        'home', // This will match the 'home' view file inside Pages
     ]);
 
-    $builder->connect('/pages/*', [
-        'controller' => 'Pages',
-        'action' => 'display',
-    ]);
+    // Custom routes
+    $builder
+        ->connect('/articles/delete/:slug', [
+            'controller' => 'Articles',
+            'action' => 'delete',
+        ])
+        ->setPass(['slug']);
 
+    $builder
+        ->connect('/articles/view/:slug', [
+            'controller' => 'Articles',
+            'action' => 'view',
+        ])
+        ->setPass(['slug']);
+
+    $builder
+        ->connect('/comments/view/:id', [
+            'controller' => 'Comments',
+            'action' => 'view',
+        ])
+        ->setPass(['id']);
+
+    $builder
+        ->connect('/comments/like/:id', [
+            'controller' => 'CommentsLikes',
+            'action' => 'add',
+        ])
+        ->setPass(['id']);
+
+    // Fallback for other controllers
     $builder->fallbacks();
 });
-
-$routes->connect('/articles/delete/:slug', [
-    'controller' => 'Articles',
-    'action' => 'delete',
-]);
-
-$routes->connect('/articles/view/:slug', [
-    'controller' => 'Articles',
-    'action' => 'view',
-]);
-
-$routes->connect('/comments/view/:id', [
-    'controller' => 'Comments',
-    'action' => 'view',
-]);
