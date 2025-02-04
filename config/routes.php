2a7d41a5dae2c71cfd17750bdd449839f4af162f
Xaii24 <110148784+Xaii24@users.noreply.github.com>
2025-02-04 00:48:54 -0700

    Removed PagesController route references

--- config/routes.php
+++ config/routes.php
@@ -5,17 +5,6 @@ use Cake\Routing\RouteBuilder;
 $routes->setRouteClass(DashedRoute::class);
 
 $routes->scope('/', function (RouteBuilder $builder) {
-    $builder->connect('/', [
-        'controller' => 'Pages',
-        'action' => 'display',
-        'login',
-    ]);
-
-    $builder->connect('/pages/*', [
-        'controller' => 'Pages',
-        'action' => 'display',
-    ]);
-
     $builder->fallbacks();
 });
 
